a6c887c7e99c785cfa686d01e61a967413a28d05
contrazap <contrazap@gmail.com>
2023-11-01 09:02:47 +0530

    1. Reverted to work with dotnet 7 2. Added ServerError and NotFound component

---
+++ client/src/app/errors/ServerError.tsx
@@ -0,0 +1,26 @@
+import { Container, Divider, Paper, Typography } from "@mui/material";
+import { useLocation } from "react-router-dom";
+
+export default function ServerError() {
+  const { state } = useLocation();
+
+  return (
+    <Container component={Paper}>
+      {state?.error ? (
+        <>
+          <Typography gutterBottom variant="h3" color="error">
+            {state.error.title}
+          </Typography>
+          <Divider />
+          <Typography variant="body1">
+            {state.error.detail || "Internal server error"}
+          </Typography>
+        </>
+      ) : null}
+
+      <Typography variant="h5" gutterBottom>
+        Server Error
+      </Typography>
+    </Container>
+  );
+}
